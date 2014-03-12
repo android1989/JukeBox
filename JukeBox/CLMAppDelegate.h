dc467be55b73e72e7563838166996151d76797f2
Andrew Hulsizer <andrew@tastemade.com>
2014-03-12 14:36:10 -0500

    Initial Commit

---
+++ JukeBox/CLMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLMAppDelegate.h
+//  JukeBox
+//
+//  Created by Andrew Hulsizer on 3/12/14.
+//  Copyright (c) 2014 Classy Monsters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JukeBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JukeBox
+//
+//  Created by Andrew Hulsizer on 3/12/14.
+//  Copyright (c) 2014 Classy Monsters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLMAppDelegate class]));
+    }
+}
