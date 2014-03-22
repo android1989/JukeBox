08402f604e3b5d3973130e8dac95d8fe96b7bc77
phillyqueso <pjacobs@xeekr.com>
2014-03-22 13:25:05 -0500

    reworked oauth to get beats to return data from the /audio call

---
+++ JukeBox/Utilities/CLMCutATrack.m
@@ -0,0 +1,59 @@
+//
+//  CLMCutTrack.m
+//  Jukebox
+//
+//  Created by Ricky Thomas on 3/13/14.
+//  Copyright (c) 2014 Classy Monsters. All rights reserved.
+//
+
+#import "CLMCutATrack.h"
+#import <AVFoundation/AVComposition.h>
+#import <AVFoundation/AVCompositionTrack.h>
+#import <AVFoundation/AVCompositionTrackSegment.h>
+#import <AVFoundation/AVMediaFormat.h>
+#import <AVFoundation/AVAssetExportSession.h>
+
+@implementation CLMCutTrack
+
++ (void) cutTrack:(NSString *)file toFile:(NSString *)outputURL startingAt:(float)start forDuration:(float)duration withCompletion:(cutCompletionBlock)completionBlock {
+    NSError *error;
+    AVMutableComposition *voyager = [AVMutableComposition composition];
+    
+    NSURL *audioURL = [[NSBundle mainBundle] URLForResource:file withExtension:@"mp3"];
+    
+    AVURLAsset* audioAsset1 = [[AVURLAsset alloc] initWithURL:audioURL options:nil];
+    AVMutableCompositionTrack *compositionAudioTrack = [voyager addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+    [compositionAudioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero,CMTimeMakeWithSeconds(duration, kCMTimeMaxTimescale))
+                                   ofTrack:[[audioAsset1 tracksWithMediaType:AVMediaTypeAudio]objectAtIndex:0]
+                                    atTime:CMTimeMakeWithSeconds(start, kCMTimeMaxTimescale)
+                                     error:&error];
+    
+    if (error) {
+        NSLog(@"%@",error);
+    } else {
+        AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:[compositionAudioTrack asset] presetName:nil];
+        
+        NSArray *pathComponents = [NSArray arrayWithObjects:
+                                   [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                                   [NSString stringWithFormat:@"%@.mp3", outputURL], nil];
+        NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+        exportSession.outputURL = outputFileURL;
+        
+        [exportSession exportAsynchronouslyWithCompletionHandler:^{
+            AVAssetExportSessionStatus exportStatus = exportSession.status;
+            
+            switch (exportStatus) {
+                case AVAssetExportSessionStatusCompleted: {
+                    completionBlock();
+                    break;
+                }
+                default: {
+                    NSLog(@"err: %@", exportSession.error);
+                    break;
+                }
+            }
+        }];
+    }
+}
+
+@end
